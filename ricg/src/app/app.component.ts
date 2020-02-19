45003dc7ab26058ad15d88af1e5f5a6be4200189
Soma Froghyar <froghyar.soma@gmail.com>
2020-02-19 01:06:51 +0000

    data reader firsts

--- ricg/src/app/app.component.ts
+++ ricg/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ItaEngCurse } from './interfaces/ita-eng-curse.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ricg';
+export class AppComponent implements OnInit {
+  data: string;
+  theStructure: ItaEngCurse[] = [];
+
+  constructor(public http: HttpClient) {}
+  ngOnInit() {
+    const options = {responseType: 'text' as 'json' };
+    this.http.get<string>('assets/data.txt', options)
+      .subscribe(
+          data => {
+              this.data = data;
+              console.log(data);
+              this.createStructure(data);
+              console.log(this.theStructure);
+          },
+          error => {
+              console.log(error);
+          }
+      );
+  }
+  createStructure(data: string) {
+    const lines = data.match(/[^\r\n]+/g);
+    lines.forEach(line => {
+      this.theStructure.push({
+        italian: line.split(',')[0],
+        english: line.split(',')[1]
+      });
+    });
+  }
 }
